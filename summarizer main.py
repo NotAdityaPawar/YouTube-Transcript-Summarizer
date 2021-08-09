2a40926f91894e034131c20cedd2b4b20d2e7009
arvind pawar <pawar97ankur@outlook.com>
2021-08-09 19:42:03 +0530

    Second commit

--- summarizer main.py
+++ summarizer main.py
@@ -1,5 +1,23 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+import PySimpleGUI as sg
+import nltk
 
 url = "https://www.youtube.com/watch?v=UF8uR6Z6KLc"
 
-video_id = url.spl
+video_id = url.split("=")[1]
+
+#print(video_id)
+
+transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+final_text = ""
+
+for line in transcript:
+    for key in line:
+        if key=="text":
+            final_text += line[key]
+
+#print(final_text)
+
+
+
